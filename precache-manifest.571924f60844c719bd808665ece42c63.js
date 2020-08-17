2f52c70ebac6729f64cac774dd17d214acc59496
heplayskeys <heplayskeys@gmail.com>
2020-08-17 11:02:50 -0700

    Updates

---
+++ precache-manifest.571924f60844c719bd808665ece42c63.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26ad4201f940060e52b1c067ae279f87",
+    "url": "/robodex/index.html"
+  },
+  {
+    "revision": "d11e6f89146207415390",
+    "url": "/robodex/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e5d6695775c9f61058ad",
+    "url": "/robodex/static/css/main.5105d350.chunk.css"
+  },
+  {
+    "revision": "d11e6f89146207415390",
+    "url": "/robodex/static/js/2.a421afd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robodex/static/js/2.a421afd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5d6695775c9f61058ad",
+    "url": "/robodex/static/js/main.b368ad0e.chunk.js"
+  },
+  {
+    "revision": "07594d1dabbd772789a3",
+    "url": "/robodex/static/js/runtime-main.b674caba.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robodex/static/media/SEGA.12a3db8b.woff"
+  }
+]);
